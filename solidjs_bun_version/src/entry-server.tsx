00cca5728f443b0a0fd231a0d81f15637d969cb0
aliouuuw <wadealiou00@gmail.com>
2026-02-16 14:59:16 +0000

    feat: integrate Apollo.io website tracking script
    Add Apollo.io tracking initialization to server-side rendered head for visitor analytics and lead tracking.

--- solidjs_bun_version/src/entry-server.tsx
+++ solidjs_bun_version/src/entry-server.tsx
@@ -14,6 +14,12 @@ export default createHandler(() => (
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&family=JetBrains+Mono:wght@400;700&display=swap" rel="stylesheet" />
           {assets}
+          <script>
+            {`function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+o.onload=function(){window.trackingFunctions.onLoad({appId:"6990aa4bcdc03a00197a72bc"})},
+document.head.appendChild(o)}initApollo();`}
+          </script>
         </head>
         <body>
           <div id="app">{children}</div>
