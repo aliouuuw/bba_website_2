935a30af7531b4946d61f44fcaf7bdc4d292b5d8
aliouuuw <wadealiou00@gmail.com>
2026-05-16 16:15:31 +0000

    chore: update package dependencies to specific versions and add SOC 2 compliance image

--- solidjs_bun_version/src/components/ComplianceBadge.tsx
+++ solidjs_bun_version/src/components/ComplianceBadge.tsx
@@ -4,7 +4,7 @@ export default function ComplianceBadge() {
       <div class="container">
         <div class="compliance-content">
           <a href="/privacy" title="SOC 2 Type II Certified - Effective March 31, 2026" class="compliance-badge-link">
-            <img src="/SOC.jpeg" alt="SOC 2 Type II Compliant" class="compliance-badge-image" />
+            <img src="/SOC_2.jpeg" alt="SOC 2 Type II Compliant" class="compliance-badge-image" />
           </a>
         </div>
       </div>
